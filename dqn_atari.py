3e818d335f2431d84576490bf0e8e6289802273e
Ontree <wangyicmh@163.com>
2017-04-01 20:54:14 -0400

    modify dueling

--- dqn_atari.py
+++ dqn_atari.py
@@ -7,7 +7,7 @@ import random
 import numpy as np
 import tensorflow as tf
 from keras.layers import (Activation, convolutional, Dense, Flatten, Input,
-                          Merge, Embedding, Reshape, Permute, Reshape, multiply, Lambda)
+                          Merge, Embedding, Reshape, Permute, Reshape, multiply, Lambda, RepeatVector)
 from keras.models import Model, Sequential
 from keras.optimizers import Adam
 from keras.layers.merge import add
@@ -73,9 +73,11 @@ def create_model(window, input_shape, num_actions, is_linear,
         if model_type == 'dueling':
             fc_layer2 = Dense(512, activation='relu')(flatten_layer)
             v_layer = Dense(1)(fc_layer1)
+            v_layer_repeated = RepeatVector(num_actions)(v_layer)
+            v_layer_repeated = Flatten()(v_layer_repeated)
             a_layer_tmp = Dense(num_actions, activation='relu')(fc_layer2)
             a_layer_processed = Lambda(lambda x: x[:, :] - K.mean(x[:, :], keepdims = True))(a_layer_tmp)
-            action_layer = add([v_layer, a_layer_processed])
+            action_layer = add([v_layer_repeated, a_layer_processed])
         else:
             action_layer = Dense(num_actions)(fc_layer1)
     else:
